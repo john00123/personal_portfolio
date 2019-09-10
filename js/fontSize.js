0cccae861c6186bd21952df5d8dd265db8dc01ae
John <john.torres@nutanix.com>
2019-09-09 17:07:24 -0700

    end of day monday

--- js/fontSize.js
+++ js/fontSize.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
-   $('#font-sizing').on('input',function(){
+   $('#font-sizing').on('change',function(){
      let size = $(this).val();
      $('.sizing').css("font-size",`${size}pt`);
+    $('#font-sizing-label').text(`${size}pt`);
    })
 });
