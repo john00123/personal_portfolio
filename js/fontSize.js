fb0bd337d6b343efa143abb786a903b886e708c8
John Torres <john.torres.hurtado@gmail.com>
2019-09-12 14:26:03 -0700

    added a couple of thing for the font use case.

--- js/fontSize.js
+++ js/fontSize.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
    $('#font-sizing').on('change',function(){
      let size = $(this).val();
      $('.sizing').css("font-size",`${size}pt`);
-    $('#font-sizing-label').text(`${size}pt`);
    })
+
+
 });
