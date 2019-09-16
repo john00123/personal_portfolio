98f44341881cc17753653cba28051cb529130cb0
John Torres <john.torres.hurtado@gmail.com>
2019-09-16 12:36:48 -0700

    smoother transitions

--- js/suggested.js
+++ js/suggested.js
@@ -3,20 +3,11 @@ $(document).ready(function() {
   $('footer a').on('click', function(event){
     event.preventDefault()
     const href = $(this).attr('href')
-
     window.history.pushState(null, null, href)
 
 
-    $.ajax({
-      url: href,
-      success: function(data){
-        $('body').fadeOut(250, function(){
-          let newPage = $(data).filter('body').html()
-          $('body').html(newPage)
-
-          $('body').fadeIn(250)
-        })
-      }
+    $('body').fadeOut(250, function(){
+      $('body').load(href).fadeIn(250)
     })
 
 
