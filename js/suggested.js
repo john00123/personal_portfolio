7b344ddc79682ac089b7ed0060b47b928ce29b6f
John Torres <john.torres.hurtado@gmail.com>
2019-09-17 16:24:13 -0700

    commented out  troublesome script for page transition

--- js/suggested.js
+++ js/suggested.js
@@ -1,23 +1,23 @@
-$(document).ready(function() {
-
-  $('footer a, .tile a').on('click', function(event){
-    event.preventDefault()
-    const href = $(this).attr('href')
-    window.history.pushState({url: href}, null, href)
-
-
-    $('body').fadeOut(250, function(){
-      $('body').load(href).scrollTop(0).fadeIn(250)
-    })
-  })
-});
-
-$(window).on('popstate', function (e) {
-    var state = e.originalEvent.state;
-    if (state !== null) {
-      $('body').load(state.url)
-    }
-    if(state === null){
-      $('body').load('index.html')
-    }
-});
+// $(document).ready(function() {
+//
+//   $('footer a, .tile a').on('click', function(event){
+//     event.preventDefault()
+//     const href = $(this).attr('href')
+//     window.history.pushState({url: href}, null, href)
+//
+//
+//     $('body').fadeOut(250, function(){
+//       $('body').load(href).scrollTop(0).fadeIn(250)
+//     })
+//   })
+// });
+//
+// $(window).on('popstate', function (e) {
+//     var state = e.originalEvent.state;
+//     if (state !== null) {
+//       $('body').load(state.url)
+//     }
+//     if(state === null){
+//       $('body').load('index.html')
+//     }
+// });
